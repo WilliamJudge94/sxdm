6ee3ea7cab9a37cd74aee7df2833100d213ffded
WilliamJudge94 <WilliamJudge94@gmail.com>
2019-07-16 13:57:09 -0500

    Updates

--- mda_import.m
+++ mda_import.m
@@ -3,7 +3,7 @@ function mda_import(loadmda_path,mda_path,detector_channel_limit,output_path)
 %addpath '/home/will/Research/UIC/SXDM/Analysis';
 %addpath '/home/will/Desktop/Processed_Data/SXDM_First_Cylce_NMC/Beam_Line_Data^/SXDM/APS_Filename^/2-Sample/MDA';
 
-%Create output_file if it doesnt exist
+% Create output_file if it doesnt exist
 if exist(output_path) == 2; 
 else
     fid = fopen(output_path,'wt');
@@ -12,17 +12,17 @@ end
 
 
 addpath (loadmda_path);
-%Create a loading bar for the user
+% Create a loading bar for the user
 bar = waitbar(1,'Program is Running, Please Wait');
 counter = 1;
 
-%collect all the mda files in the path
+% Collect all the mda files in the path
 mda_files = dir(mda_path);
 its_pre = size(mda_files);
 its = its_pre(1);
 filenames ={};
 
-%Grab the list of all the mda files
+% Grab the list of all the mda files
 for i = (3:its)
     full_name = horzcat(mda_path,'/',mda_files(i).name);
     filenames = [filenames, full_name];  
@@ -34,7 +34,7 @@ total_amount = total_amount_pre(2);
 user_input = input('Type In Scan Number To Import Or Type - 0 - To Import Entire Folder '); 
 user_input_corr = sprintf('%04d',user_input);
 
-%For each file open up all the channels and store values in hdf5
+% For each file open up all the channels and store values in hdf5
 for filename = filenames
     mda_file = filename{1}(end-7:end-4);
     if user_input_corr == '0000'
@@ -48,11 +48,11 @@ for filename = filenames
         loading_str = ['Loading Scan - ' mda_file ' -  Please Wait' ];
         waitbar((counter-1)/total_amount,bar,loading_str);
         counter = counter + 1;
-        %For each detector channel
+        % For each detector channel
         for detector_channel = (1:detector_channel_limit)
-            %create a string of the detector challen for the h5 file
+            % Create a string of the detector challen for the h5 file
             detector_string = strcat('D',sprintf('%02d',detector_channel));
-            %try to loadmda for that detector
+            % Try to loadmda for that detector
             disp(detector_channel)
             try
                 data=loadmda(filename{1},detector_channel,'n','n'); 
@@ -65,8 +65,8 @@ for filename = filenames
                 dim1 = shape_data2(1);
                 dim2 = shape_data2(2);
 
-            %if you can load it try to save the data to the appropriate
-            %location in h5 file
+            % If you can load it try to save the data to the appropriate
+            % Location in h5 file
                 group = strcat('/mda/',mda_file,'/',detector_string);
                 try
                     h5create(output_path,group,[dim1 dim2]);  
