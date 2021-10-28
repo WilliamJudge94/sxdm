f4f3296d83a25e8e1e249627cb2bbfdc08a1374d
WilliamJudge94 <37912237+WilliamJudge94@users.noreply.github.com>
2021-10-28 14:02:26 -0500

    Updating autodocs try 3

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,9 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 import sphinx_rtd_theme
